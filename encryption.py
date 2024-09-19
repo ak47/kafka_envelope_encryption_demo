3b1d2a846b2b3c8dad10627b8ebb846572639744
Andrew Koch <andrew.koch@hingehealth.com>
2024-09-19 14:08:58 -0700

    Add example using OPENSSL to generate the plaintext data encryption key (DEK).
    Allow both OPENSSL or KMS options for DEK generation based on global var.
    
    Add usage of authentication tag for correct GCM implementation.

--- encryption.py
+++ encryption.py
@@ -13,6 +13,7 @@ import boto3
 from botocore.exceptions import ClientError
 from logging import getLogger
 from os import urandom
+import secrets
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
@@ -31,8 +32,9 @@ getLogger("boto3").setLevel("CRITICAL")
 kms_client = boto3.client("kms", region_name=region)
 
 # AWS CMK Key ARN
-KEY_ID = "< your key here >"
+KEY_ID = "<Your Key ARN>"
 TOPIC = "envelope_encryption_demo"
+DATA_ENCRYPTION_KEY_GENERATOR = "OPENSSL"
 
 def encrypt_data(plaintext, key_id):
     """
@@ -41,19 +43,31 @@ def encrypt_data(plaintext, key_id):
     """
     # Generate a random IV
     iv = urandom(16)
-
+    plaintext_data_key: bytes
     try:
-        # Generate a data key using AWS CMK
-        data_key = kms_client.generate_data_key(KeyId=key_id, KeySpec="AES_256")
+        if DATA_ENCRYPTION_KEY_GENERATOR == "KMS":
+            # Generate a data key using AWS KMS
+            data_key = kms_client.generate_data_key(KeyId=key_id, KeySpec="AES_256")
+            plaintext_data_key = data_key["Plaintext"]
+        elif DATA_ENCRYPTION_KEY_GENERATOR == "OPENSSL":
+            # Generate a data key using OpenSSL
+            plaintext_data_key = secrets.token_bytes(32)
+
+            # encrypt the data key with AWS KMS
+            data_key: dict = kms_client.encrypt(
+                KeyId=key_id, Plaintext=plaintext_data_key
+            )
     except ClientError as e:
         getLogger().error(e)
         exit()
 
+
     # Encrypt the data with AES CBC mode
-    plaintext_data_key = data_key["Plaintext"]
+
     encrypted_data_key = data_key["CiphertextBlob"]
-    cipher = Cipher(algorithms.AES(plaintext_data_key), modes.CBC(iv))
+    cipher = Cipher(algorithms.AES(plaintext_data_key), modes.GCM(iv))
     encryptor = cipher.encryptor()
+
 
     padder = padding.PKCS7(128).padder()
     padded_data = padder.update(plaintext)
@@ -72,29 +86,30 @@ def encrypt_data(plaintext, key_id):
         encrypted_data_key,
         encoded_encrypted_data_key,
         iv,
+        encryptor.tag,
     )
 
 
-def decrypt_data(encoded_ciphertext_blob, encoded_encrypted_data_key, iv):
+def decrypt_data(encoded_ciphertext_blob: bytes, encoded_encrypted_data_key: bytes, iv: bytes, gcm_tag: bytes) -> bytes:
     """
     Method to decrypt the data
     The method returns the decrypted data
     """
     # Base64 decode of encrypted data and encrypted data key
-    decoded_ciphertext_blob = base64.b64decode(encoded_ciphertext_blob)
-    decoded_encrypted_data_key = base64.b64decode(encoded_encrypted_data_key)
+    decoded_ciphertext_blob: bytes = base64.b64decode(encoded_ciphertext_blob)
+    decoded_encrypted_data_key: bytes = base64.b64decode(encoded_encrypted_data_key)
 
     # Decrypt the data key
-    data_key = kms_client.decrypt(CiphertextBlob=decoded_encrypted_data_key)
-    plaintext_data_key = data_key["Plaintext"]
+    data_key: dict = kms_client.decrypt(CiphertextBlob=decoded_encrypted_data_key)
+    plaintext_data_key: bytes = data_key["Plaintext"]
 
     # Decrypt the data
-    cipher = Cipher(algorithms.AES(plaintext_data_key), modes.CBC(iv))
+    cipher = Cipher(algorithms.AES(plaintext_data_key), modes.GCM(iv))
     decryptor = cipher.decryptor()
-    decrypted_padded_data = decryptor.update(decoded_ciphertext_blob) + decryptor.finalize()
+    decrypted_padded_data: bytes = decryptor.update(decoded_ciphertext_blob) + decryptor.finalize_with_tag(gcm_tag)
 
     unpadder = padding.PKCS7(128).unpadder()
-    plaintext = unpadder.update(decrypted_padded_data)
+    plaintext: bytes = unpadder.update(decrypted_padded_data)
     plaintext += unpadder.finalize()
 
     # Return the decrypted data
@@ -119,11 +134,12 @@ def main():
         encrypted_data_key,
         encoded_encrypted_data_key,
         iv,
+        gcm_tag
     ) = encrypt_data(input_plaintext, KEY_ID)
 
     print(encoded_ciphertext_blob)
     # Decrypt the data
-    plaintext = decrypt_data(encoded_ciphertext_blob, encoded_encrypted_data_key, iv)
+    plaintext = decrypt_data(encoded_ciphertext_blob, encoded_encrypted_data_key, iv, gcm_tag)
     print(json.loads(str(plaintext, "utf-8")))
 
     # Store the encrypted string and the encrypted data key in a file (optional)
